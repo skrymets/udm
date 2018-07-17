06a75e0a44b315f3394621a3c12b561958ebbd29
Sergii Krymets <skrymets@gmail.com>
2018-07-17 15:14:12 +0300

    Add additional Contact Mechanism types

---
+++ src/main/java/udm/contacts/TelecomunicationNumber.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 skrymets.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package udm.contacts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class TelecomunicationNumber extends ContactMechanism {
+
+    private static final long serialVersionUID = -9053143176615595246L;
+
+    @Column(nullable = true)
+    private String countryCode;
+
+    @Column(nullable = true)
+    private String areaCode;
+
+    @Column(nullable = false)
+    private String number;
+
+    public TelecomunicationNumber() {
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
